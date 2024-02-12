9f99dbdc97eed1fb3b4835e2cf69cb9997ecb7e4
shelton1007 <72802575+shelton1007@users.noreply.github.com>
2024-02-12 12:30:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,6 +30,24 @@ def mongraphique():
 @app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
+@app.route('/commits/')
+def commits():
+    # Appel à l'API GitHub pour récupérer les données sur les commits
+    response = requests.get('https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits')
+    commits_data = response.json()
+
+    # Analyse des données pour compter les commits par minute
+    commits_per_minute = {}
+    for commit in commits_data:
+        date_string = commit['commit']['author']['date']
+        minute = date_string[14:16]  # Extraire les minutes de la date
+        commits_per_minute[minute] = commits_per_minute.get(minute, 0) + 1
+
+    # Création de listes de minutes et de quantités de commits pour le graphique
+    minutes = list(commits_per_minute.keys())
+    commits_count = list(commits_per_minute.values())
+
+    return render_template('commits.html', minutes=minutes, commits_count=commits_count)
 
 if __name__ == "__main__":
   app.run(debug=True)
